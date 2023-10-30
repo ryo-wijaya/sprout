bdd7c395c41e8d741ab695d1016282d5b43a851d
Ryo <ryo200013@gmail.com>
2023-10-30 14:44:22 +0800

    remove SimpleTestStore files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
-const SimpleTestStore = artifacts.require("SimpleTestStore");
 const User = artifacts.require("User");
 const JobListing = artifacts.require("JobListing");
 const NativeToken = artifacts.require("NativeToken");
 
 module.exports = async (deployer, network, accounts) => {
-  await deployer.deploy(SimpleTestStore);
   await deployer.deploy(User);
   await deployer.deploy(NativeToken);
   await deployer.deploy(JobListing, User.address, NativeToken.address);
