b3e981ae52fdd8f9a734f320edd7fcbc0c9d7374
Haris Dzulkifli <haris@dbasri.com>
2023-11-22 02:58:48 +0800

    Wrote test cases for Job Review functions

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,13 +3,13 @@ const JobListing = artifacts.require("JobListing");
 const NativeToken = artifacts.require("NativeToken");
 // const Escrow = artifacts.require("Escrow");
 // const DisputeResolutionDAO = artifacts.require("DisputeResolutionDAO");
-// const JobReview = artifacts.require("JobReview");
+const JobReview = artifacts.require("JobReview");
 
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(User);
   await deployer.deploy(NativeToken);
-  // await deployer.deploy(Escrow);
   await deployer.deploy(JobListing, User.address, NativeToken.address);
-  // await deployer.deploy(JobReview, User.address, JobListing.address);
+  // await deployer.deploy(Escrow, User.address, NativeToken.address, JobListing.address);
+  await deployer.deploy(JobReview, User.address, JobListing.address);
   // await deployer.deploy(DisputeResolutionDAO, User.address, Escrow.address, JobListing.address);
 };
