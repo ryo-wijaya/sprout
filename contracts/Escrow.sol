7eb78581b4e0b25fdd37d736f5dfb07b1e65c334
Ryo <ryo200013@gmail.com>
2023-11-22 04:29:27 +0800

    Escrow functions for DAO

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -8,7 +8,13 @@ contract Escrow {
 
     // ===================================================== SCHEMA & STATE VARIABLES ===================================================== //
 
-    enum EscrowStatus { AWAITING_PAYMENT, COMPLETE, REFUNDED }
+    /*
+    * AWAITING_PAYMENT: The client has transferred the job reward to the escrow contract, and is awaiting freelancer job completion
+    * COMPLETE: This is a happy path. The freelancer has completed the job and the client has confirmed delivery of the job, and the job reward has been transferred to the freelancer
+    * PARTIALLY_REFUNDED: There is a dispute, the job reward has been refunded to the client or freelancer depending on its result, but the staked dispute tokens are kept
+    * REFUNDED: There is a dispute, but the escrow holds 0 balance. All relevant parties (client, freelancer, reviewers) have been paid out
+    */
+    enum EscrowStatus { AWAITING_PAYMENT, COMPLETE, PARTIALLY_REFUNDED, REFUNDED }
 
     struct Payment {
         uint256 clientId;
@@ -18,6 +24,11 @@ contract Escrow {
         uint256 balance;
         EscrowStatus status;
     }
+
+    // Constant for the number of tokens client must stake in the potential event of a dispute (business rule)
+    uint256 constant STAKED_TOKENS = 10;
+    // Constant for the number of tokens each voter gets as a reward for voting (business rule)
+    uint256 constant EACH_VOTER_REWARD = 1;
 
     uint256 public numPayments = 0;
     NativeToken nativeTokenContract;
@@ -33,7 +44,9 @@ contract Escrow {
 
     event PaymentInitiated(uint256 _paymentId, uint256 _jobId, uint256 _freelancerId, uint256 _clientId, uint256 _amount);
     event PaymentComplete(uint256 _paymentId);
+    event PaymentPartiallyRefunded(uint256 _paymentId); // This means the escrow balance is not yet 0
     event PaymentRefunded(uint256 _paymentId);
+    event VoterReward(uint256 _paymentId, address voterAddress);
 
     // Check that the freelancer and client came from different address
     modifier differentAddresses(uint256 _freelancerId, uint256 _clientId) {
@@ -99,36 +112,37 @@ contract Escrow {
 
 
     /**
-        * Function for the NativeTokens to be transferred to the Freelancer on completion of Job, with no disputes
-        * NativeToken is transferred to the Freelancer, staked 10 tokens in the event of a dispute is refunded to the client
+        * Function for the NativeTokens to be transferred to the Freelancer on completion of Job, with the option to be with or without the staked tokens
         *
         * Considerations:
         * - The payment status must be AWAITING_PAYMENT
         * - The job must be COMPLETED (already checked by the jobListing contract)
     */
-    function confirmDelivery(uint256 _paymentId) public payable validPaymentId(_paymentId) {
+    function confirmDelivery(uint256 _paymentId, bool withStakedTokens) public payable validPaymentId(_paymentId) {
         Payment storage payment = payments[_paymentId];
 
         require(payment.status == EscrowStatus.AWAITING_PAYMENT, "Invalid payment status");
 
         // Pay the freelancer
-        nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.freelancerId), payment.amount - 10);
+        nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.freelancerId), payment.amount - STAKED_TOKENS);
 
-        // Refund staked tokens to the client
-        nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.clientId), 10);
-
-        // Empty balance
-        payment.balance = 0;
-
-        // Client confirms delivery
-        payment.status = EscrowStatus.COMPLETE;
-
-        emit PaymentComplete(_paymentId);
+        if (withStakedTokens) {
+            // Refund staked tokens to the client, empty balance and mark as delivered
+            nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.clientId), STAKED_TOKENS);
+            payment.balance = 0;
+            payment.status = EscrowStatus.COMPLETE;
+            emit PaymentComplete(_paymentId);
+        } else {
+            // Do not refund staked tokens to the client, mark as partially refunded, awaiting distribution of staked tokens to voters
+            payment.balance = STAKED_TOKENS;
+            payment.status = EscrowStatus.PARTIALLY_REFUNDED;
+            emit PaymentPartiallyRefunded(_paymentId);
+        }
     }
 
 
     /**
-        * Function for the NativeTokens to be refunded --> Does this depend on any disputes?
+        * Function for the NativeTokens to be refunded to the client minus the staked tokens
         *
         * Considerations:
         * - The payment status must be AWAITING_PAYMENT
@@ -138,19 +152,36 @@ contract Escrow {
     function refundPayment(uint256 _paymentId) public validPaymentId(_paymentId) {
         Payment storage payment = payments[_paymentId];
         require(payment.status == EscrowStatus.AWAITING_PAYMENT, "Invalid payment status");
-        // require(jobContract.isJobClosed(payment.jobId) != true, "Job cannot be completed");
-        //With my current code only the client can refund payment
-        //cause only client approved to take money out of the escrow contract
-        nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.clientId), payment.amount);
-        // does this depend on the DAO contract? 
-        payment.status = EscrowStatus.REFUNDED;
 
-        //Client cannot take more money out of contract
-        nativeTokenContract.approve(msg.sender, 0);
-        emit PaymentRefunded(_paymentId);
-
+        nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.clientId), payment.amount - STAKED_TOKENS);
+        payment.balance = STAKED_TOKENS;
+        payment.status = EscrowStatus.PARTIALLY_REFUNDED;
+        emit PaymentPartiallyRefunded(_paymentId);
     }
 
+    function rewardVoter(uint256 _paymentId, address voterAddress) public validPaymentId(_paymentId) {
+        Payment storage payment = payments[_paymentId];
+        require(payment.status == EscrowStatus.PARTIALLY_REFUNDED, "Invalid payment status");
+
+        if (payment.balance > 0) {
+            nativeTokenContract.transferFrom(address(this), voterAddress, EACH_VOTER_REWARD);
+            payment.balance--;
+            emit VoterReward(_paymentId, voterAddress);
+        }
+    }
+
+    function refundTokenBalance(uint256 _paymentId) public validPaymentId(_paymentId) {
+        Payment storage payment = payments[_paymentId];
+        require(payment.status == EscrowStatus.PARTIALLY_REFUNDED, "Invalid payment status");
+
+        if (payment.balance > 0) {
+            nativeTokenContract.transferFrom(address(this), userContract.getAddressFromUserId(payment.clientId), payment.balance);
+            payment.balance = 0;
+        }
+
+        payment.status = EscrowStatus.REFUNDED;
+        emit PaymentRefunded(_paymentId);
+    }
 
     // Getter functions    
     function getClientId(uint256 _paymentId) public view returns (uint256) {
